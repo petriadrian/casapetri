d93ca08f8188bd24994399cdbb09ff340c07aca3
petri <petriadrian@gmail.com>
2018-04-04 10:32:18 +0300

    first comit 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,73 @@
-import { Component } from '@angular/core';
+import {Component, HostListener, OnInit, Pipe, PipeTransform} from '@angular/core';
+import {DomSanitizer, SafeHtml} from '@angular/platform-browser';
+import {UrlService} from './url-services/url.service';
+import * as $ from 'jquery';
+import {NavigationEnd, Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  constructor(private urlService: UrlService, private router: Router) {
+    // google analytics
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        console.log('gogl analy sent: ' + event.urlAfterRedirects);
+        (<any>window).ga('set', 'page', event.urlAfterRedirects);
+        (<any>window).ga('send', 'pageview');
+      }
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  public onScrollEvent($event) {
+    if (this.urlService.isHostOnBrowser()) {
+      if (window.pageYOffset > 300) {
+        $('#stickHeaderBarOnTheTop').fadeIn(200);
+        $('#stickPageIntroMenuOnTheTop').addClass('showPageIntroBar').fadeIn();
+      } else {
+        $('#stickHeaderBarOnTheTop').fadeOut();
+        $('#stickPageIntroMenuOnTheTop').removeClass('showPageIntroBar');
+      }
+    }
+  }
+
 }
+
+@Pipe({name: 'safeUrl'})
+export class SafeUrl implements PipeTransform {
+  constructor(private sanitizer: DomSanitizer) {
+  }
+
+  transform(url) {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+}
+
+@Pipe({name: 'trustHtml'})
+export class TrustHtml implements PipeTransform {
+  constructor(private _sanitizer: DomSanitizer) {
+  }
+
+  transform(v: string): SafeHtml {
+    return this._sanitizer.bypassSecurityTrustHtml(v);
+  }
+}
+
+@Pipe({name: 'removeHtmlFromText'})
+export class RemoveHtmlFromText implements PipeTransform {
+  constructor() {
+  }
+
+  transform(text) {
+    return text ? String(text).replace(/<[^>]+>/gm, '') : '';
+  }
+}
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,59 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 
 
-import { AppComponent } from './app.component';
-
+import {AppComponent, RemoveHtmlFromText, SafeUrl, TrustHtml} from './app.component';
+import {HeaderWebSiteComponent} from './header-web-site/header-web-site.component';
+import {FooterWebSiteComponent} from './footer-web-site/footer-web-site.component';
+import {FormComponent} from './body/section/form/form.component';
+import {FormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
+import {ROUTES} from './app.router';
+import {UrlService} from './url-services/url.service';
+import {ArticleTitleComponent} from './body/section/article-title/article-title.component';
+import {ArticleComponent} from './body/section/article/article.component';
+import {LinkDetailBoxComponent} from './body/section/link-detail-box/link-detail-box.component';
+import {CarouselComponent} from './body/section/carousel/carousel.component';
+import {HeadlineComponent} from './body/section/headline/headline.component';
+import {SectionManagementComponent} from './body/section-management/section-management.component';
+import {ArticleBodyComponent} from './body/section/article/article-body/article-body.component';
+import {CommonArticleComponent} from './body/section/article/common-article/common-article.component';
+import {LinkComponent} from './body/section/link/link.component';
+import {SocialButtonsComponent} from './body/section/social-buttons/social-buttons.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SafeUrl,
+    TrustHtml,
+    RemoveHtmlFromText,
+    HeaderWebSiteComponent,
+    FooterWebSiteComponent,
+    FormComponent,
+    ArticleTitleComponent,
+    ArticleComponent,
+    LinkDetailBoxComponent,
+    CarouselComponent,
+    HeadlineComponent,
+    SectionManagementComponent,
+    ArticleBodyComponent,
+    CommonArticleComponent,
+    LinkComponent,
+    SocialButtonsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule.withServerTransition({appId: 'casapetri'}),
+    FormsModule,
+    RouterModule.forRoot(ROUTES), // to display logs add {enableTracing: true} as 2nd param,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [UrlService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private urlService: UrlService) {
+    const platform = urlService.isHostOnBrowser() ? 'in the browser' : 'on the server';
+    console.log(`Running ${platform}`);
+  }
+}
