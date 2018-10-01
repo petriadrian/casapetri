0075f9d0474d9323422216fe6389235ed21b1899
petri <petriadrian@gmail.com>
2018-10-01 20:14:39 +0300

    index facebook iframe

--- src/app/footer-web-site/footer-web-site.component.ts
+++ src/app/footer-web-site/footer-web-site.component.ts
@@ -1,8 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {UrlService} from '../url-services/url.service';
-import { Inject, Injectable } from '@angular/core';
-import { DOCUMENT } from '@angular/common';
+import {Inject, Injectable} from '@angular/core';
+import {DOCUMENT} from '@angular/common';
 
 @Injectable()
 @Component({
@@ -15,7 +15,7 @@ export class FooterWebSiteComponent implements OnInit {
   public footerContent;
   public reviews;
 
-  constructor(private http: HttpClient, private changeUrlService: UrlService, @Inject(DOCUMENT) private document: any) {
+  constructor(private http: HttpClient, private changeUrlService: UrlService) {
   }
 
   ngOnInit() {
@@ -27,15 +27,17 @@ export class FooterWebSiteComponent implements OnInit {
     const reviewsArticlesPath = './assets/content/' + this.changeUrlService.getCurrentLang() + '/common/articles/reviews.json';
     this.http.get(reviewsArticlesPath).subscribe(res => this.reviews = res);
 
-    // needed to load facebook iframe
-    const fjs = this.document.getElementsByTagName('script')[0];
-    if (this.document.getElementById('facebook-jssdk')) {
-      return;
+    if (this.changeUrlService.isHostOnBrowser()) {
+      // needed to load facebook iframe
+      const fjs = document.getElementsByTagName('script')[0];
+      if (document.getElementById('facebook-jssdk')) {
+        return;
+      }
+      const js = document.createElement('script');
+      js.id = 'facebook-jssdk';
+      js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.11&appId=1018480531572788&autoLogAppEvents=1';
+      fjs.parentNode.insertBefore(js, fjs);
     }
-    const js = this.document.createElement('script');
-    js.id = 'facebook-jssdk';
-    js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.11&appId=1018480531572788&autoLogAppEvents=1';
-    fjs.parentNode.insertBefore(js, fjs);
   }
 
   public buildLinkAndGo(link) {
